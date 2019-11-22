76b9993179658de32bc0a940afcc955140308fe0
helmutkemper <helmut.kemper@ahgora.com.br>
2019-11-22 11:20:51 -0300

    trabalhando

--- typeCoordinate.go
+++ typeCoordinate.go
@@ -16,15 +16,46 @@ func (el *Coordinate) Set(value int) {
 	}
 
 	el.OriginalValue = value
-	el.DensityValue = float64(value) * el.DensityFactor
+	el.DensityValue = float64(el.OriginalValue) * el.DensityFactor
+}
+
+func (el *Coordinate) Add(value int) {
+	if el.DensityFactor == 0 {
+		el.DensityFactor = 1
+	}
+
+	el.OriginalValue += value
+	el.DensityValue = float64(el.OriginalValue) * el.DensityFactor
+}
+
+func (el *Coordinate) Sub(value int) {
+	if el.DensityFactor == 0 {
+		el.DensityFactor = 1
+	}
+
+	el.OriginalValue -= value
+	el.DensityValue = float64(el.OriginalValue) * el.DensityFactor
 }
 
 func (el *Coordinate) SetDensityFactor(value float64) {
 	el.DensityFactor = value
+	el.DensityValue = float64(el.OriginalValue) * el.DensityFactor
 }
 
 func (el Coordinate) Int() int {
+	return int(el.DensityValue)
+}
 
+func (el Coordinate) Float64() float64 {
+	return el.DensityValue
+}
+
+func (el Coordinate) Float32() float32 {
+	return float32(el.DensityValue)
+}
+
+func (el Coordinate) Float() float64 {
+	return el.DensityValue
 }
 
 func (el Coordinate) String() string {
